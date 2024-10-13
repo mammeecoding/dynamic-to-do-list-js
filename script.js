3d63943f0212da9946e136fd287dc4e565419f72
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-10-13 13:44:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -36,4 +36,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
    }
  });
+//added
+   function loadTasks() {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    storedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
+}
+
+function addTask(taskText, save = true) {
+    
+
+    if (save) {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        storedTasks.push(taskText);
+        localStorage.setItem('tasks', JSON.stringify(storedTasks));
+    }
+}
+  
 });
